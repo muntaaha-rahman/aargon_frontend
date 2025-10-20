17f18ba23fa0898011259c0125afa30a2968dce4
muntaaha-rahman <muntaaha3007@gmail.com>
2025-10-20 15:43:01 +0600

    config fixed

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,7 +1,3 @@
-// export const SERVER_CONFIG = {
-//   BASE_URL: 'http://10.220.220.100:8000/api/v1',
-// };
-
 export const SERVER_CONFIG = {
-  BASE_URL: 'http://0.0.0.0:8000/api/v1',
+  BASE_URL: 'http://10.220.220.100:8000/api/v1',
 };
